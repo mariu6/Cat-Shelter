ad183810b9f71401760d842db951f7a827e9fa7f
Mariusz Ilijew <mariu6@gmail.com>
2020-05-24 22:57:15 +0200

    The rest of the files.

---
+++ handlers/home.js
@@ -0,0 +1,34 @@
+const url = require("url");
+const fs = require("fs");
+const path = require("path");
+const cats = require("../data/cats.json");
+const breeds = require("../data/breeds.json");
+
+let filePath = path.normalize(
+    path.normalize(__dirname, "../views/home/index.js")
+);
+
+module.exports = (req, res) => {
+    const pathname = url.parse(req.url).pathname;
+
+    if (pathname === "/" && req.method === "GET") {
+        fs.readFile(filePath, (error, data) => {
+            if (error) {
+                console.log(error);
+                res.writeHead(404, {
+                    "Content-Type": "text/plain"
+                });
+                res.write("404 Not Found!!!\n");
+                res.end();
+                return;
+            }
+            res.writeHead(200, {
+                "Content-Type": "text/html"
+            });
+            res.send(data);
+            res.end();
+        });
+    } else {
+        return true;
+    }
+}
