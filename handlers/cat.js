029c49696dd085eed4d3cea5dc062c023c642cdd
Mariusz Ilijew <mariu6@gmail.com>
2020-05-27 14:42:57 +0200

    Upload of new cat

--- handlers/cat.js
+++ handlers/cat.js
@@ -14,7 +14,11 @@ module.exports = (req, res) => {
         let filePath = path.normalize(path.join(__dirname, "../views/addCat.html"));
         const input = fs.createReadStream(filePath);   // fs.readFile() can be used as well
 
-        input.on("data", (data) => res.write(data));
+        input.on("data", (data) => {
+            let catBreedPlaceholder = breedsFile.map((breed) => `<option value="${breed}">${breed}</option>`).join("");    // It acts like 
+            let modifiedData = data.toString().replace('{{catBreeds}}', catBreedPlaceholder);                              // proto handlebars 
+            res.write(modifiedData);
+        });
         input.on("end", () => res.end());
         input.on("error", (err) => console.log(err));
 
@@ -26,8 +30,8 @@ module.exports = (req, res) => {
         input.on("end", () => res.end());
         input.on("error", (err) => console.log(err));
 
-        } else if (pathname === "/cats/add-breed" && req.method === "POST") {     // 0. Fill the HTML with action="/cats/add-breed" method="POST"
-            console.log("opo");
+    } else if (pathname === "/cats/add-breed" && req.method === "POST") {     // 0. Fill the HTML with action="/cats/add-breed" method="POST"
+        console.log("opo");
         let filePath = path.normalize(path.join(__dirname, "../data/breeds.json"));
         console.log(filePath);
         let formData = "";                                             // 1. Parse the incoming data from the form
@@ -55,7 +59,30 @@ module.exports = (req, res) => {
         });
 
     } else if (pathname === "/cats/add-cat" && req.method === "POST") {
+        let form = new formidable.IncomingForm();           // form is used for processing various form data 
+        form.parse(req, (err, fields, files) => {           // fields - passing info from form fields, files - from preset formidable
+            if (err) throw err;
 
+            // move of the uploaded file (in this case - picture)
+            let oldPath = files.upload.path;                         // taking them from the default formidable folder
+            let newPath = path.normalize(path.join(__dirname, "../content/images/" + files.upload.name));  // creating new path in my folder
+            fs.rename(oldPath, newPath, (err) => {                   // transfer     
+                if (err) throw err;
+                console.log("Files was uploaded sccessfully!");
+            });
+
+            // Add new object to cats.json
+            fs.readFile("./data/cats.json", "utf-8", (err, data) => {   
+                if (err) throw err;
+                let allCats = JSON.parse(data);                         // parsing to array of objects
+                allCats.push({ id: catsFile.length + 1, ...fields, image: files.upload.name });   // constructing and adding the new object
+                let json = JSON.stringify(allCats);                     // set back to JSON
+                fs.writeFile("./data/cats.json", json, () => {          // rewrite the original file with the new cat info
+                    res.writeHead(301, { location: "/" });              // redirect
+                    res.end();
+                })
+            })
+        })
 
     } else {
         return true;
